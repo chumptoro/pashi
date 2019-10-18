e7418110448aa74505068c2857ad0080856cf59d
mark <mark.pham.developer@gmail.com>
2019-10-17 22:55:22 -0700

    signin.html

--- app.py
+++ app.py
@@ -3,8 +3,14 @@ import os
 
 from datetime import datetime
 
-from flask import Flask, render_template, request, redirect, url_for
+
+from flask import Flask, render_template, request, redirect, url_for, session
+
+
 app = Flask(__name__)
+
+app.secret_key = 'babyiloveyoubutthatsnotenough'
+
 
 from bson.objectid import ObjectId
 
@@ -12,6 +18,34 @@ from bson.objectid import ObjectId
 client = MongoClient()
 db = client.ContractorProject
 purchases = db.purchases
+users = db.users
+
+'''
+from flask_login import LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
+'''
+
+'''
+@app.route('/account')
+def account_details():
+    show details of user
+    if users.find_one({"username": session['username'], "password": session['password']}) == None:
+        message = 'Please sign in or sign up first'
+        return redirect(url_for('signin', message=message))
+    else:
+        displays purchases associated with this account
+'''
+
+
+@app.route('/signin')
+def signin():
+    username_attempt = request.form.get('username')
+    password_attempt = request.form.get('password')
+    if users.find_one({'username': username_attempt, 'password': password_attempt}) == None:
+        message = 'incorrect login info.  please try again'
+        return render_template('signin.html', message=message)
+    else:
 
 
 @app.route('/')
@@ -74,7 +108,7 @@ def cart():
             'delivery_address': request.form.get('delivery_address'),
             'email': request.form.get('email'),
         }
-        print("marco reus")
+        # print("marco reus")
         print(purchase)
         purchase_id = purchases.insert_one(purchase).inserted_id
         return redirect(url_for('purchase_show', purchase_id=purchase_id))
